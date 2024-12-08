76e211c0b511e554a75b81f7b13037b7c814624f
gao1983 <qingchaogao763@gmail.com>
2024-12-08 14:48:43 +0800

    add sushu

---
+++ testing.py
@@ -0,0 +1,9 @@
+for i in range(2,101):
+    Is_prime = True #假设条件成立
+    for j in range(2,i):
+        if i % j == 0:
+            Is_prime = False
+    #else:
+    #    print(f"{i}是素数")
+    if Is_prime:
+        print(f"{i}素数")
